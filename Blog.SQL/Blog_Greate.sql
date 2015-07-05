087ae4b4c25ec96f0448d91c46c4c3435932b780
AnnaSheremet <Sherxxx@gmail.com>
2015-07-06 00:31:56 +0400

    added request for table creation Article

--- Blog.SQL/Blog_Greate.sql
+++ Blog.SQL/Blog_Greate.sql
@@ -4,8 +4,6 @@ GO
 USE DB_Blog
 GO
 
---Створення таблиць
-
 CREATE TABLE Users(
 	ID_User INT IDENTITY(1,1) PRIMARY KEY,
 	Logins VARCHAR(25) NOT NULL,
@@ -13,12 +11,17 @@ CREATE TABLE Users(
 	Surname VARCHAR(50),
 	Passwords CHAR(12) NOT NULL,
 	Date_of_birth DATE,
-	roles VARCHAR(6) NOT NULL CHECK (roles IN('guest','user','admin')) DEFAULT 'user',
+	roles VARCHAR(6) NOT NULL CHECK (roles IN('user','admin')) DEFAULT 'user',
 	Gender varchar(10) NOT NULL CHECK (Gender IN('female', 'male')),
-	Date_of_registration DATE NOT NULL		)
+	Date_of_registration DATE NOT NULL	
+)
 
 CREATE TABLE Aticle(
-	Id int not null indentity (1,1),
-	AuthoreId int not null,
-	Title nvarchar()
+	Id int not null IDENTITY (1,1) PRIMARY KEY,
+	AuthoreId int not null foreign key REFERENCES Users(ID_User),
+	Title nvarchar(255) not null,
+	CreationTime dateTime null,
+	Content nvarchar(max) null,
+	Published bit not null,
+	Comment bit not null
 )
--- Blog.SQL/Blog_Insert.sql
+++ Blog.SQL/Blog_Insert.sql
@@ -6,4 +6,8 @@ VALUES ('Sher', 'Anna', 'Sheremet', '123456', NULL, 'admin', 'female', '27-06-20
 	   ('AA', 'AAA', 'AAAAA', '123456', '26-07-1990', 'user', 'male', '28-06-2015' ),
 	   ('BB', 'BBB', 'BBBBB', '123456', '02-09-1980', 'user', 'male', '28-06-2015' );
 
-
+INSERT INTO Aticle
+VALUES ( 1, 'Sport', GETDATE(), NULL, 1, 1),
+	   ( 2, 'Food', GETDATE(), NULL, 0, 1),
+	   ( 2, 'Погода', GETDATE(), NULL, 1, 1),
+	   ( 3, 'Моє місто', GETDATE(), NULL, 1, 1);
